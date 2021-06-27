935f51e429684a1ec79afade07a20a85c7b3793c
Sangam Shrestha <2shrestha22@gmail.com>
2021-06-27 20:46:33 +0545

    initial commit

---
+++ dt.js
@@ -0,0 +1,3 @@
+var d=new Date()
+var year=d.getFullYear()
+document.write(+year)
---
+++ ua.js
@@ -0,0 +1,10 @@
+var c=navigator.appCodeName;
+var n=navigator.appName;
+var v=navigator.appVersion;
+var ce=navigator.cookieEnabled;
+var p=navigator.platform;
+var u=navigator.userAgent;
+var pr=navigator.product;
+var l=navigator.language;
+var j=navigator.javaEnabled;
+document.write("<p><b>Browser Code Name: </b>"+c+"<p><b>Browser Name: </b>"+n+"<p><b>Browser Version: </b>"+v+"<p><b>Cookies Enabled: </b>"+ce+"<p><b>Java Enabled: </b>"+j+"<p><b>Language: </b>"+l+"<p><b>Product: </b>"+pr+"<p><b>Platform: </b>"+p+"<p><b>User-agent Header: </b>"+u)
